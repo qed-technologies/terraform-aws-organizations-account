e05d0d7d6a545bf2b23dcd2ce6e7bf176ceb34be
GreenyMcDuff <chris.greenwood@qedtechnologies.uk>
2020-12-12 17:17:23 +0000

    Fix permissions boundary length issue

--- iam.tf
+++ iam.tf
@@ -1,3 +1,16 @@
+locals {
+  # Multi-step process to ensure path is prefixed and 
+  # suffixed with a "/"
+  iam_namespace_first_check = substr(var.iam_namespace, 0, 1) != "/" ? "/${var.iam_namespace}" : var.iam_namespace
+  iam_namespace_final       = substr(local.iam_namespace_first_check, length(local.iam_namespace_first_check) - 1, 1) != "/" ? "${local.iam_namespace_first_check}/" : local.iam_namespace_first_check
+  # These service permissions are added to the 
+  # permissions boundary policy
+  permitted_service_actions = [for service in var.permitted_services : "${service}:*"]
+  # Required to allow specific Service Linked Roles
+  # to be created
+  permitted_service_names = [for service in var.permitted_services : "${service}.amazonaws.com"]
+}
+
 #------------------
 # Password policy
 #------------------
@@ -19,66 +32,44 @@ resource "aws_iam_account_password_policy" "this" {
 #-------------------
 # IAM manager role
 #-------------------
-resource "aws_iam_role" "this" {
-  provider = aws.member
-  count    = var.create_iam_manager_role ? 1 : 0
+module "iam_role_iam_manager" {
+  source = "github.com/qed-technologies/terraform-aws-iam-role?ref=v2.1.0"
+
+  providers = {
+    aws = aws.member
+  }
+
+  create = var.create_iam_manager_role
 
   name        = "IamManager"
   description = "Deployed by the Organization to manage IAM resource in this account"
+  path        = local.iam_namespace_final
 
-  assume_role_policy   = data.aws_iam_policy_document.iam_mangaer_trust.json
-  permissions_boundary = aws_iam_policy.permissions_boundary[count.index].arn
+  trusted_identities       = var.iam_manager_trusted_identities
+  permissions_boundary_arn = aws_iam_policy.permissions_boundary[0].arn
+
+  managed_policy_arns = [
+    aws_iam_policy.iam_manager[0].arn,
+    aws_iam_policy.terraform_state_write.arn
+  ]
 
   tags = var.tags
 }
 
-# Trust policy
-data "aws_iam_policy_document" "iam_mangaer_trust" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = var.iam_manager_trusted_identities
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-# Managed policy
-resource "aws_iam_policy" "customer" {
+# --------------------
+# IAM Manager policy
+# --------------------
+resource "aws_iam_policy" "iam_manager" {
   provider = aws.member
   count    = var.create_iam_manager_role ? 1 : 0
 
   name        = "IamManager"
   description = "Allows management of IAM entities in this account"
+  path        = local.iam_namespace_final
   policy      = data.aws_iam_policy_document.iam_manager.json
 }
 
-resource "aws_iam_role_policy_attachment" "customer" {
-  provider = aws.member
-  count    = var.create_iam_manager_role ? 1 : 0
-
-  role       = aws_iam_role.this[0].name
-  policy_arn = aws_iam_policy.customer[count.index].arn
-}
-
 data "aws_iam_policy_document" "iam_manager" {
-  statement {
-    sid    = "PreventBoundaryUpdates"
-    effect = "Deny"
-
-    actions = [
-      "iam:CreatePolicyVersion",
-      "iam:DeletePolicy",
-      "iam:DeletePolicyVersion",
-      "iam:SetDefaultPolicyVersion"
-    ]
-
-    resources = ["arn:aws:iam::${aws_organizations_account.account.id}:policy/OrgBoundary"]
-  }
-
   statement {
     sid    = "EnforceIamBoundary"
     effect = "Allow"
@@ -100,136 +91,210 @@ data "aws_iam_policy_document" "iam_manager" {
       values   = ["arn:aws:iam::${aws_organizations_account.account.id}:policy/OrgBoundary"]
     }
   }
-
-  statement {
-    sid    = "ManageTerraformStateLock"
-    effect = "Allow"
-
-    actions = [
-      "dynamodb:GetItem",
-      "dynamodb:PutItem",
-      "dynamodb:DeleteItem"
-    ]
-
-    resources = [aws_dynamodb_table.terraform.arn]
-  }
-
-  statement {
-    sid    = "ProtectTerraformStateLockTable"
-    effect = "Deny"
-
-    actions = [
-      "dynamodb:DeleteTable"
-    ]
-
-    resources = [aws_dynamodb_table.terraform.arn]
-  }
-
-  statement {
-    sid    = "ReadTerraformStateBucket"
-    effect = "Allow"
-
-    actions = [
-      "s3:ListBucket"
-    ]
-
-    resources = [aws_s3_bucket.terraform.arn]
-  }
-
-  statement {
-    sid    = "ReadTerraformState"
-    effect = "Allow"
-
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject"
-    ]
-
-    resources = ["${aws_s3_bucket.terraform.arn}/*"]
-  }
-
-  statement {
-    sid    = "ProtectTerraformStateBucket"
-    effect = "Deny"
-
-    actions = [
-      "s3:DeleteBucket",
-      "s3:PutBucketVersioning"
-    ]
-
-    resources = [aws_s3_bucket.terraform.arn]
-  }
-
-  statement {
-    sid    = "ProtectTerraformState"
-    effect = "Allow"
-
-    actions = [
-      "s3:DeleteObject"
-    ]
-
-    resources = ["${aws_s3_bucket.terraform.arn}/*"]
-  }
 }
 
-
-# -----------------------------------------------
+# -----------------------------
 # Permissions boundary policy
-# Prevents the IamManager role from 
-# being able to create aditional IAM entities 
-# without the permissions boundary attached
-# -----------------------------------------------
+# -----------------------------
 resource "aws_iam_policy" "permissions_boundary" {
   provider = aws.member
   count    = var.create_iam_manager_role ? 1 : 0
 
   name        = "OrgBoundary"
-  path        = "/"
+  path        = local.iam_namespace_final
   description = "Organization permissions boundary"
   policy      = data.aws_iam_policy_document.permissions_boundary.json
 }
 
 data "aws_iam_policy_document" "permissions_boundary" {
-  source_json = var.permissions_boundary_policy
-
   statement {
-    sid    = "PreventBoundaryUpdates"
+    sid = "PreventBoundaryRemoval"
+
     effect = "Deny"
 
     actions = [
-      "iam:CreatePolicyVersion",
-      "iam:DeletePolicy",
-      "iam:DeletePolicyVersion",
-      "iam:SetDefaultPolicyVersion"
+      "iam:DeleteRolePermissionsBoundary"
     ]
 
-    resources = ["arn:aws:iam::${aws_organizations_account.account.id}:policy/OrgBoundary"]
+    resources = [
+      "arn:aws:iam::${aws_organizations_account.account.id}:role/*"
+    ]
   }
 
   statement {
-    sid    = "EnforceIamBoundary"
+    sid = "PreventOrgManagedResourceUpdates"
+
+    effect = "Deny"
+
+    actions = [
+      # Policies
+      "iam:CreatePolicyVersion",
+      "iam:DeletePolicy",
+      "iam:DeletePolicyVersion",
+      "iam:SetDefaultPolicyVersion",
+      # Terraform State
+      "dynamodb:DeleteTable",
+      "s3:DeleteBucket",
+      "s3:DeleteObject",
+      "s3:PutBucketVersioning",
+      # Terraform State Encryption
+      "kms:CancelKeyDeletion",
+      "kms:DeleteAlias",
+      "kms:DisableKey",
+      "kms:DisableKeyRotation",
+      "kms:GetKeyPolicy",
+      "kms:GetKeyRotationStatus",
+      "kms:PutKeyPolicy",
+      "kms:ScheduleKeyDeletion",
+      "kms:UntagResource",
+      "kms:UpdateAlias"
+    ]
+
+    resources = [
+      # Policies
+      "arn:aws:iam::${aws_organizations_account.account.id}:policy/${var.iam_namespace}/*",
+      # Terraform State
+      aws_dynamodb_table.terraform.arn,
+      aws_s3_bucket.terraform.arn,
+      "${aws_s3_bucket.terraform.arn}/*",
+      # Terraform State Encryption
+      aws_kms_key.terraform.arn,
+      aws_kms_alias.terraform.arn
+    ]
+  }
+
+  statement {
+    sid    = "ManageTerraformRemoteState"
+    effect = "Allow"
+
+    actions = [
+      # State Lock
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
+      # State File
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutObject",
+      # State Encryption
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+
+    resources = [
+      # State Lock
+      aws_dynamodb_table.terraform.arn,
+      # State File
+      aws_s3_bucket.terraform.arn,
+      "${aws_s3_bucket.terraform.arn}/*",
+      # State Encryption
+      aws_kms_key.terraform.arn
+    ]
+  }
+
+  statement {
+    sid = "EnforceIamBoundary"
+
     effect = "Allow"
 
     actions = [
       "iam:AttachRolePolicy",
       "iam:CreateRole",
+      "iam:DeleteRole",
       "iam:DeleteRolePolicy",
       "iam:DetachRolePolicy",
       "iam:PutRolePermissionsBoundary",
       "iam:PutRolePolicy"
     ]
 
-    resources = ["*"]
+    resources = [
+      "*"
+    ]
 
     condition {
       test     = "StringEquals"
       variable = "iam:PermissionsBoundary"
-      values   = ["arn:aws:iam::${aws_organizations_account.account.id}:policy/OrgBoundary"]
+      values = [
+        "arn:aws:iam::${aws_organizations_account.account.id}:policy/${var.iam_namespace}/*"
+      ]
     }
   }
 
   statement {
-    sid    = "ManageTerraformStateLock"
+    sid = "AllowIamManagement"
+
+    effect = "Allow"
+
+    #checkov:skip=CKV_AWS_49:Wildcards on read-only actions are acceptable
+    actions = [
+      "iam:AddRoleToInstanceProfile",
+      "iam:CreateInstanceProfile",
+      "iam:DeleteInstanceProfile",
+      "iam:Get*",
+      "iam:List*",
+      "iam:RemoveRoleFromInstanceProfile"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid = "ManageServiceLinkedRoles"
+
+    effect = "Allow"
+
+    actions = [
+      "iam:ServiceLinkedRole*"
+    ]
+
+    resources = [
+      "arn:aws:iam::${aws_organizations_account.account.id}:role/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "iam:AWSServiceName"
+      values   = local.permitted_service_names
+    }
+  }
+
+  statement {
+    sid = "AllowedServices"
+
+    effect = "Allow"
+
+    #checkov:skip=CKV_AWS_1:False positive this doesn't allow full admin
+    actions = local.permitted_service_actions
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+# -----------------------------------------------
+# Terraform state policy
+# Can be attached to roles in the account to 
+# allow IAM entities to write Terraform State
+# -----------------------------------------------
+resource "aws_iam_policy" "terraform_state_write" {
+  provider = aws.member
+
+  name        = "TerraformStateWrite"
+  path        = local.iam_namespace_final
+  description = "Permissions required to write Terraform STate"
+  policy      = data.aws_iam_policy_document.terraform_state_write.json
+}
+
+data "aws_iam_policy_document" "terraform_state_write" {
+  statement {
+    sid = "ManageTerraformStateLock"
+
     effect = "Allow"
 
     actions = [
@@ -238,33 +303,28 @@ data "aws_iam_policy_document" "permissions_boundary" {
       "dynamodb:DeleteItem"
     ]
 
-    resources = [aws_dynamodb_table.terraform.arn]
-  }
-
-  statement {
-    sid    = "ProtectTerraformStateLockTable"
-    effect = "Deny"
-
-    actions = [
-      "dynamodb:DeleteTable"
+    resources = [
+      aws_dynamodb_table.terraform.arn
     ]
-
-    resources = [aws_dynamodb_table.terraform.arn]
   }
 
   statement {
-    sid    = "ReadTerraformStateBucket"
+    sid = "ReadTerraformStateBucket"
+
     effect = "Allow"
 
     actions = [
       "s3:ListBucket"
     ]
 
-    resources = [aws_s3_bucket.terraform.arn]
+    resources = [
+      aws_s3_bucket.terraform.arn
+    ]
   }
 
   statement {
-    sid    = "ReadTerraformState"
+    sid = "ReadTerraformState"
+
     effect = "Allow"
 
     actions = [
@@ -272,29 +332,8 @@ data "aws_iam_policy_document" "permissions_boundary" {
       "s3:PutObject"
     ]
 
-    resources = ["${aws_s3_bucket.terraform.arn}/*"]
-  }
-
-  statement {
-    sid    = "ProtectTerraformStateBucket"
-    effect = "Deny"
-
-    actions = [
-      "s3:DeleteBucket",
-      "s3:PutBucketVersioning"
+    resources = [
+      "${aws_s3_bucket.terraform.arn}/*"
     ]
-
-    resources = [aws_s3_bucket.terraform.arn]
-  }
-
-  statement {
-    sid    = "ProtectTerraformState"
-    effect = "Allow"
-
-    actions = [
-      "s3:DeleteObject"
-    ]
-
-    resources = ["${aws_s3_bucket.terraform.arn}/*"]
   }
 }
