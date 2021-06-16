0bc29f3b82df3d0bf7e2187503393d9d1e85a1e4
GreenyMcDuff <chris.greenwood@qedtechnologies.uk>
2021-06-16 09:46:01 +0100

    test

--- terraform_resources.tf
+++ terraform_resources.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket" "terraform" {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm     = "aws:kms"
-        kms_master_key_id = aws_kms_key.terraform.arn
+        kms_master_key_id = aws_kms_key.terraform[0].arn
       }
     }
   }
